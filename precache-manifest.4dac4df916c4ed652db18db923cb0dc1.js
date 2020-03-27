1346b1c35d77bc229f823defc7f0476844fb7ffe
Rahmad Subakti <rahmadsubakti@github.com>
2020-03-28 01:47:17 +0800

    Updates

---
+++ precache-manifest.4dac4df916c4ed652db18db923cb0dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37b951406f0e97bef5258b151da8161",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "a1b3b11ebd58da49e774",
+    "url": "/pokedex/static/css/main.55c90cd6.chunk.css"
+  },
+  {
+    "revision": "c084262326d846e73b18",
+    "url": "/pokedex/static/js/2.3e4096b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.3e4096b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b3b11ebd58da49e774",
+    "url": "/pokedex/static/js/main.dcf3893e.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  }
+]);
